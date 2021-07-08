907fd8c3044a070df41f9ab81f90fb3ea539cf76
Luis_DELACRUZ <ldelacruz@igp.gob.pe>
2021-07-08 17:52:31 -0500

    some changes

---
+++ src/other/get_inputData.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This program obtains:
+#   - MeasEpoch2
+#   - ChannelStatus
+# data from an SBF file 
+# -------------------------------
+INPUT_PATH='/media/cesar/Septentrio02/Binary/'
+OUTPUT_PATH='/home/cesar/Desktop/luisd/scripts/Graficas_desvanecimientos/Input_data/Data_set/'
+OUTPUT_PATH2='/home/cesar/Desktop/luisd/scripts/Graficas_desvanecimientos/Input_data/Data_binary/'
+CONVERTER='/opt/Septentrio/RxTools/bin/bin2asc'
+
+FOLDERS=`diff -q ${INPUT_PATH} ${OUTPUT_PATH2} | grep Only | grep ${INPUT_PATH} | awk '{print $4}'`
+
+if [[ -z $FOLDERS ]]
+then 
+  echo "There isn't any new file yet!"
+else
+  echo "The new files are:"
+  echo $FOLDERS
+  for FOLDER in $FOLDERS
+  do
+    # Get MeasEpoch2 ascii file  
+    ls ${INPUT_PATH}${FOLDER}/*_ | xargs -t -I % ${CONVERTER} -f % -m MeasEpoch2 -t -x -i 1
+    mv ${INPUT_PATH}${FOLDER}/*MeasEpoch*.txt ${OUTPUT_PATH} 
+    
+    # Get ChannelStatus ascii file  
+    ls ${INPUT_PATH}${FOLDER}/*_ | xargs -t -I % ${CONVERTER} -f % -m ChannelStatus -t -x -i 1
+    mv ${INPUT_PATH}${FOLDER}/*ChannelStatus*.txt ${OUTPUT_PATH} 
+    mkdir ${OUTPUT_PATH2}${FOLDER}
+  done
+  echo "All binary files were converted sucesfully!"
+fi
+
--- src/plot_CN0.py
+++ src/plot_CN0.py
@@ -1,43 +1,37 @@
-#!/home/luis/anaconda3/bin/python3
+#!/usr/bin/python3.6
 #___________________________________________________________
 #                  AMPLITUDE vs TIME 
 #                        plots
-#                        v1.0
+#                        v2.0
 #-----------------------------------------------------------
 # This script creates Amplitude vs Time plots, for each 
-# frequency and constellation. SBAS data is included in 
-# GPS const by default. Elevation vs Time plots are also 
-# included in the same graphs. Finally, these plots are 
-# saved in an A4 pdf file.
+# frequency and constellation. Sampling time (Ts) is 
+# increased to 30s, with the min & max values over a 
+# period of minute. SBAS data is included in GPS const 
+# by default. Elevation vs Time plots are also included 
+# in the same graphs. These plots are saved in an A4 pdf 
+# file.
+# INPUT FILES: MeasEpoch2 & ChannelStatus ASCCI FILES.   
 # Author: Luis D.
-# :)
-from septentrio_tools import ProcessISMR, PlotsISMR
+# :):)
 from matplotlib.backends.backend_pdf import PdfPages
+from septentrio_tools import ProcessSBF as sbf
+from septentrio_tools import PlotsISMR
 import glob 
 import os 
 
 # Declare input and output paths 
-root_path = "/home/luis/Desktop/Proyects_Files/LISN/GPSs/Tareas/Graficas_desvanecimientos/"
+root_path = "/home/cesar/Desktop/luisd/scripts/Graficas_desvanecimientos/"
 input_files_path = root_path + "Input_data/Data_set/"
 input_files_path_op = root_path + "Input_data/Data_procesada/"
 output_files_path = root_path + "Output_data/"
-file_name = "ljic2810.20_.ismr" # Test file 
 
-def process_dataframe(input_file):
-    file1 = ProcessISMR()
-    # Read and normalize the df
-    file1.read_file(input_file)
-    file1.normalize_df()
-    # Rename the Elev column
-    file1.rename_column(5, "Elev")
-    # Rename the CN0 columns
-    file1.rename_column(6, "CN0_sig1")
-    file1.rename_column(31, "CN0_sig2")
-    file1.rename_column(45, "CN0_sig3")
-    # Extract certain columns 
-    file1.extract_columns(cols=["Elev", "CN0_sig1", "CN0_sig2", "CN0_sig3"])
-    # Convert to float
-    file1.convert2float(cols=["Elev", "CN0_sig1", "CN0_sig2", "CN0_sig3"])
+def process_dataframe(input_file_cn, input_file_elv):
+    file1 = sbf()
+    file1.read_channelStatus(input_file_elv)
+    file1.read_measEpoch(input_file_cn)
+    df = file1.get_ampElev()
+    #print(df.head())
     # Filter df based on elevation col 
     for j in range(3):
         j += 1
@@ -46,36 +40,44 @@ def process_dataframe(input_file):
     df = file1.df
     print("df ready to plot!")
 
-    return df 
+    return df
 
 def main():
     # Specify the const and freq to plot    
-    const_list = ['G', 'E']   
-    freq_list = ['CN0_sig1', 'CN0_sig2', 'CN0_sig3'] 
+    const_freq_list = {'G':['CN0_sig1', 'CN0_sig2', 'CN0_sig3'],
+                       'E':['CN0_sig1', 'CN0_sig2']}
 
-    list_input_files = glob.glob(input_files_path + "*.ismr")
+    list_aux = glob.glob(input_files_path + "*.txt")
+    list_aux2 = [file[len(input_files_path): len(input_files_path) + 17] for file in list_aux]
+    list_input_files = list(set(list_aux2))
+    list_input_files.sort() # e.g. ['ljic3240.20__SBF_', 'ljic3250.20__SBF_']
+
     if len(list_input_files) > 0:
         for file_i in list_input_files:
             # Get the df
-            df = process_dataframe(file_i)
+            file_cn = input_files_path + file_i + "MeasEpoch2.txt"
+            file_elv =  input_files_path + file_i + "ChannelStatus.txt"
+            df = process_dataframe(file_cn, file_elv)
             
             # Plot
-            input_file_name = file_i[len(input_files_path):]
-            g1 = PlotsISMR(dataframe=df, ismr_file_name=input_file_name)
+            #input_file_name = file_i[len(input_files_path):]
+            g1 = PlotsISMR(dataframe=df, ismr_file_name=file_i)
             # -> Create an empty pdf file to save the plots
             figure_name = g1.get_output_figure_name() + "_CN0.pdf" # e.g. ljic_200806_CN0.pdf
             pdf = PdfPages(output_files_path + figure_name)
             # -> Generate the plots
-            for c in const_list:
-                for f in freq_list:         
-                    g1.plotCN0(const=c, freq=f, pdf=pdf)
+            for const, freqs in const_freq_list.items():
+                for freq in freqs:         
+                    g1.plotCN0(const=const, freq=freq, pdf=pdf)
             pdf.close()
 
             # Move input files to a permanent directory
-            os.rename(file_i, input_files_path_op + input_file_name)
+            os.rename(file_cn, input_files_path_op + file_i + "MeasEpoch2.txt")
+            os.rename(file_elv, input_files_path_op + file_i + "ChannelStatus.txt")
     
     return 'Ok'
 
 if __name__ == '__main__':
+    print("STARTING...")
     main()
     print("FINISHED ----------")
